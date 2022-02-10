6f4d19c3e841547767618e4caf32251912ab6623
Fergasi <fergusmagor@gmail.com>
2022-02-09 22:08:17 -0500

    Version2

--- string-repeater.js
+++ string-repeater.js
@@ -5,8 +5,11 @@ var n = prompt("Enter a random string of letters: ");
 let z = n;
 
 while (z.length < 10){
+     
+     console.log(z)
+     
      z = z.concat(n);
-
+     
      }    
 
 console.log(z);
